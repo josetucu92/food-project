00881a8861fa2370a9d7444bc0ba4b09e31209ef
unknown <josemanuel_seguro2@hotmail.com>
2022-07-03 20:21:09 -0300

    getAllRecipes working with Promise.all

--- api/src/controllers/recipe.controllers.js
+++ api/src/controllers/recipe.controllers.js
@@ -5,44 +5,91 @@ import { Recipe, Diet } from '../database/db.js';
 
 
 
-const getApiInfo = async () => {
-    const apiInfo = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=${amountRecipes}`);
-    const apiInfoRecipes = apiInfo.data.results.map(el => {
-        return {
-            id: el.id,
-            name: el.title,
-            image: el.image,
-            diets: el.diets?.map(diet => diet),
-            summary: el.summary,
-            healthScore: el.healthScore,
-            dishTypes: el.dishTypes?.map(dish => dish),
-            steps: el.analyzedInstructions[0]?.steps.map(el => {
-                return {
-                    number: el.number,
-                    steps: el.steps
-                }
-            })
-        }
-    })
-    return apiInfoRecipes
-};
+// const getApiInfo = async () => {
+//     const apiInfo = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=${amountRecipes}`);
+//     // const apiInfoRecipes = apiInfo.data.results.map(el => {
+//     //     return {
+//     //         id: el.id,
+//     //         name: el.title,
+//     //         image: el.image,
+//     //         diets: el.diets?.map(diet => diet),
+//     //         summary: el.summary,
+//     //         healthScore: el.healthScore,
+//     //         dishTypes: el.dishTypes?.map(dish => dish),
+//     //         steps: el.analyzedInstructions[0]?.steps.map(el => {
+//     //             return {
+//     //                 number: el.number,
+//     //                 steps: el.steps
+//     //             }
+//     //         })
+//     //     }
+//     // })
+//     // return apiInfoRecipes
+//     return apiInfo.data.results
+// };
 
-const getDbRecipes = async () => {
-    return await Recipe.findAll({
-        include: {
-            model: Diet,
+// const getDbRecipes = async () => {
+//     return await Recipe.findAll({
+//         include: {
+//             model: Diet,
+//             attributes: ['name'],
+//             through: {
+//                 attributes: []
+//             },
+//         }
+//     })
+// };
+
+
+// export const getAllRecipes = async () => {
+//     const apiInfo = await getApiInfo()
+//     const dbInfo = await getDbRecipes()
+//     const allInfo = apiInfo.concat(dbInfo)
+//     return allInfo
+// };
+
+export const getAllRecipes = (req, res, next) => {
+    try {
+        const apiRecipesPromise =  axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=${amountRecipes}`);
+        
+        const dbRecipesPromise =  Recipe.findAll({
+                include: {
+                    model: Diet,
             attributes: ['name'],
             through: {
                 attributes: []
-            },
-        }
-    })
-};
+                    },
+                    row: true
+            }
+        });
 
+        Promise.all([apiRecipesPromise, dbRecipesPromise])
+            .then(response => {
+                const apiRecipes = response[0].data.results.map(el => {
+                            return {
+                                id: el.id,
+                                name: el.title,
+                                image: el.image,
+                                diets: el.diets?.map(diet => diet),
+                                summary: el.summary,
+                                healthScore: el.healthScore,
+                                dishTypes: el.dishTypes?.map(dish => dish),
+                                steps: el.analyzedInstructions[0]?.steps.map(el => {
+                                    return {
+                                        number: el.number,
+                                        steps: el.steps
+                                    }
+                                })
+                            }
+                        })
 
-export const getAllRecipes = async () => {
-    const apiInfo = await getApiInfo()
-    const dbInfo = await getDbRecipes()
-    const allInfo = apiInfo.concat(dbInfo)
-    return allInfo
-};
+                const dbRecipes = response[1];
+
+                const allRecipes = [...apiRecipes, ...dbRecipes];
+
+                res.status(200).send(allRecipes)
+            })
+    } catch (error) {
+        next(error)
+    }
+}
--- api/src/routes/recipe.routes.js
+++ api/src/routes/recipe.routes.js
@@ -5,15 +5,18 @@ import { getAllRecipes } from '../controllers/recipe.controllers.js';
 
 const router = Router()
 
-router.get('/', async (req, res, next) => {
-    try {
-        const getRecipes = await getAllRecipes()
-        res.status(200).send(getRecipes)
-    } catch (error) {
-        next(error)
-    }
+
+router.get('/', getAllRecipes);
+
+// router.get('/', async (req, res, next) => {
+//     try {
+//         const getRecipes = await getAllRecipes()
+//         res.status(200).send(getRecipes)
+//     } catch (error) {
+//         next(error)
+//     }
         
-});
+// });
 
 
 router.post('/', async (req, res, next) => {
