c04979d7252ea91f39bb877196bd13d220b3be33
josetucu92 <josemanuel_seguro2@hotmail.com>
2023-03-24 12:41:46 -0300

    feat/set up landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import LandingPage from './components/Landing/LandingPage';
-import CreateRecipe from './components/CreateRecipe/CreateRecipe';
-import Details from './components/Details/Details';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import { LandingPage } from "./pages/Landing/LandingPage";
+import CreateRecipe from "./components/CreateRecipe/CreateRecipe";
+import Details from "./components/Details/Details";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='main-App'>
+      <div className="main-App">
         <Routes>
-          <Route exact path='/' element={<LandingPage/>} />
-          <Route exact path='/home' element={<Home/>} />
-          <Route exact path='/home/:id' element={<Details/>} />
-          <Route exact path='/create-recipe' element={<CreateRecipe/>} />
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/home/:id" element={<Details />} />
+          <Route exact path="/create-recipe" element={<CreateRecipe />} />
         </Routes>
       </div>
     </BrowserRouter>
