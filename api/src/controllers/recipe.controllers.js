4a05a1b3e063801eeb95ed815e63305e66249666
Jose Manuel Seguro <josemanuel_seguro2@hotmail.com>
2023-05-31 22:01:32 -0300

    feat: general improves (#4)

--- api/src/controllers/recipe.controllers.js
+++ api/src/controllers/recipe.controllers.js
@@ -1,116 +1,118 @@
-import axios from 'axios';
-import { apiKey, apiKey2, apiKey3, apiKey4, apiKey5 } from '../utils/index.js';
-import { amountRecipes } from '../utils/index.js';
-import { Recipe, Diet } from '../database/db.js';
-
+import axios from "axios";
+import { apiKey, apiKey2, apiKey3, apiKey4, apiKey5 } from "../utils/index.js";
+import { amountRecipes } from "../utils/index.js";
+import { Recipe, Diet } from "../database/db.js";
 
 export const getApiRecipes = async () => {
-try {
-    const apiRecipesPromise = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey5}&addRecipeInformation=true&number=${amountRecipes}`);
-    const apiRecipes = apiRecipesPromise.data?.results.map(el => {
-        return {
-                            id: el.id.toString(),
-                            name: el.title,
-                            image: el.image,
-                            Diets: el.diets?.map(diet => diet),
-                            summary: el.summary,
-                            healthScore: el.healthScore,
-                            dishTypes: el.dishTypes?.map(dish => dish),
-                            steps: (
-                                //el.analyzedInstructions[0] && el.analyzedInstructions[0].steps ? 
-                                el.analyzedInstructions[0]?.steps?.map(item => item.step))
-                        }
-                    })
-                    //console.log(apiRecipes[0].steps)
+  const apiKeys = apiKey || apiKey2 || apiKey3 || apiKey4 || apiKey5;
+  try {
+    const apiRecipesPromise = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKeys}&addRecipeInformation=true&number=${amountRecipes}`
+    );
+    const apiRecipes = apiRecipesPromise.data?.results.map((el) => {
+      return {
+        id: el.id.toString(),
+        name: el.title,
+        image: el.image,
+        Diets: el.diets?.map((diet) => diet),
+        summary: el.summary,
+        healthScore: el.healthScore,
+        dishTypes: el.dishTypes?.map((dish) => dish),
+        steps:
+          //el.analyzedInstructions[0] && el.analyzedInstructions[0].steps ?
+          el.analyzedInstructions[0]?.steps?.map((item) => item.step),
+      };
+    });
+    //console.log(apiRecipes[0].steps)
     return apiRecipes;
-} catch(error) {
-    console.error('Fetching the data from the API failed', error)
-}
+  } catch (error) {
+    console.error("Fetching the data from the API failed", error);
+  }
 };
 
 const dbRecipesPromise = async () => {
-    try {
-        const dbRecipes = await Recipe.findAll({
-            include: {
-                model: Diet,
-        attributes: ['name'],
+  try {
+    const dbRecipes = await Recipe.findAll({
+      include: {
+        model: Diet,
+        attributes: ["name"],
         through: {
-            attributes: []
-                },
-                row: true
-        }
-        });
-        return dbRecipes;
-    } catch (error) {
-        console.error(error)
-    }
+          attributes: [],
+        },
+        row: true,
+      },
+    });
+    return dbRecipes;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const getAllRecipes = async () => {
-    try {
-        const apiRecipes = await getApiRecipes();
-        const dbRecipes = await dbRecipesPromise();
-        const allRecipes = [...apiRecipes, ...dbRecipes];
-        return allRecipes;
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    const apiRecipes = await getApiRecipes();
+    const dbRecipes = await dbRecipesPromise();
+    const allRecipes = [...apiRecipes, ...dbRecipes];
+    return allRecipes;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // --------------------->Routes<---------------------
 
 export const getAllAndByName = async (req, res, next) => {
-    try {
-        const { name } = req.query;
-        const allRecipes = await getAllRecipes();
-        if (name) {
-            const filteredRecipes = allRecipes.filter(recipe => recipe.name.toLowerCase().includes(name.toLowerCase()));
-            filteredRecipes.length ? 
-            res.status(200).send(filteredRecipes) :
-            res.status(404).send('No recipes found with that name');
-        } else {
-            return res.status(200).send(allRecipes)
-        }
-    } catch (error) {
-        next(error)
+  try {
+    const { name } = req.query;
+    const allRecipes = await getAllRecipes();
+    if (name) {
+      const filteredRecipes = allRecipes.filter((recipe) =>
+        recipe.name.toLowerCase().includes(name.toLowerCase())
+      );
+      filteredRecipes.length
+        ? res.status(200).send(filteredRecipes)
+        : res.status(404).send("No recipes found with that name");
+    } else {
+      return res.status(200).send(allRecipes);
     }
+  } catch (error) {
+    next(error);
+  }
 };
 
-
 export const getRecipeById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const allRecipes = await getAllRecipes(id);
-        if(id) {
-            const recipeFiltered = allRecipes.find(recipe => recipe.id === id);
-            // console.log(recipeFiltered.id.length)
-            recipeFiltered.name.length ? 
-            res.status(200).send(recipeFiltered) :
-            res.status(404).send('No recipes found with that ID');
-        }
-    } catch (error) {
-        next(error)
+  try {
+    const { id } = req.params;
+    const allRecipes = await getAllRecipes(id);
+    if (id) {
+      const recipeFiltered = allRecipes.find((recipe) => recipe.id === id);
+      // console.log(recipeFiltered.id.length)
+      recipeFiltered.name.length
+        ? res.status(200).send(recipeFiltered)
+        : res.status(404).send("No recipes found with that ID");
     }
-}
-
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const postRecipe = async (req, res, next) => {
-    try {
-        const { name, summary, healthScore, steps, diets } = req.body;
-        const newRecipe = await Recipe.create({
-            name,
-            summary,
-            healthScore,
-            steps,
-            //diets
-        })
-        const findAllDiets = await Diet.findAll({
-            where: { name: diets }
-        })
-        await newRecipe.addDiet(findAllDiets)
+  try {
+    const { name, summary, healthScore, steps, diets } = req.body;
+    const newRecipe = await Recipe.create({
+      name,
+      summary,
+      healthScore,
+      steps,
+      //diets
+    });
+    const findAllDiets = await Diet.findAll({
+      where: { name: diets },
+    });
+    await newRecipe.addDiet(findAllDiets);
 
-        return res.status(200).send(newRecipe)
-    } catch (error) {
-        next(error)
-    }
+    return res.status(200).send(newRecipe);
+  } catch (error) {
+    next(error);
+  }
 };
--- client/src/components/RecipeCard/Card.jsx
+++ client/src/components/RecipeCard/Card.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Card.css";
 
-export const RecipeCard = ({ name, img, Diets, id }) => (
-  <div className="card-container">
-    <img className="card-img" src={img} alt="food plate" />
-    <h3>{name}</h3>
-    <div className="card-diets">{Diets}</div>
-    <div className="btn-container-card">
-      <Link to={"/home/" + id} className="btn-card">
-        Details
-      </Link>
+export const RecipeCard = ({ name, img, diets = [], id }) =>
+  console.log(diets) || (
+    <div className="card-container">
+      <img className="card-img" src={img} alt="food plate" />
+      <h3>{name}</h3>
+      <div className="diets-container">
+        {diets &&
+          diets.map((el) => <span style={{ width: "120px" }}>{el}</span>)}
+      </div>
+      <div className="btn-container-card">
+        <Link to={"/home/" + id} className="btn-card">
+          Details
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
